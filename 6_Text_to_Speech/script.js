a1b02f9a0d2700c144f7eb625f215b688f4fc15d
Hakan <hakan@yaman.uk>
2024-01-27 22:53:43 +0300

    First commit

--- 6_Text_to_Speech/script.js
+++ 6_Text_to_Speech/script.js
@@ -2,35 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
     let speech = new SpeechSynthesisUtterance();
     let voices = [];
     let voicesSelected = document.querySelector('#voice-selection');
-
+  
     function populateVoices() {
-        voices = window.speechSynthesis.getVoices();
-        if (voices.length > 0) {
-            voicesSelected.innerHTML = ''; // Clear existing options
-            voices.forEach((voice) => {
-                let option = new Option(voice.name, voice.name);
-                voicesSelected.appendChild(option);
-            });
-        }
+      voices = window.speechSynthesis.getVoices();
+      if (voices.length > 0) {
+        voicesSelected.innerHTML = ''; // Clear existing options
+        voices.forEach((voice) => {
+          let option = new Option(voice.name, voice.name);
+          voicesSelected.appendChild(option);
+        });
+      }
     }
-
+  
     window.speechSynthesis.onvoiceschanged = populateVoices;
     // Initial call in case voices are already loaded
     populateVoices();
-
+  
     document.querySelector('#speak-button').addEventListener('click', () => {
-        speech.text = document.querySelector('#text-to-speak').value;
-        let selectedVoiceName = voicesSelected.value;
-
-        setTimeout(() => {
-            let selectedVoice = voices.find(voice => voice.name === selectedVoiceName);
-            if (selectedVoice) {
-                speech.voice = selectedVoice;
-                window.speechSynthesis.cancel(); // Cancel any ongoing speech
-                window.speechSynthesis.speak(speech); // Speak with the new voice
-            } else {
-                console.error("Selected voice not found in available voices");
-            }
-        }, 0);
+      speech.text = document.querySelector('#text-to-speak').value;
+      let selectedVoiceName = voicesSelected.value;
+  
+      setTimeout(() => {
+        let selectedVoice = voices.find(voice => voice.name === selectedVoiceName);
+        if (selectedVoice) {
+          speech.voice = selectedVoice;
+          window.speechSynthesis.cancel(); // Cancel any ongoing speech
+          window.speechSynthesis.speak(speech); // Speak with the new voice
+        } else {
+          console.error("Selected voice not found in available voices");
+        }
+      }, 100); // Add a delay of 100 milliseconds before selecting the voice
     });
-});
+  });
